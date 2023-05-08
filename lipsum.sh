8c4a0be9bcca3f1b6ee71704f7e1d367277daba2
amayanij <amayanij87@gmail.com>
2023-05-08 11:33:55 -0300

    cambio en lipsum.sh   acomodamiento de lineas

--- lipsum.sh
+++ lipsum.sh
@@ -1,11 +1,39 @@
 #!/bin/bash
 
-echo "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.">loremipsum-1.txt
+echo "Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+ Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+ when an unknown printer took a galley of type and scrambled it to make a type specimen book.">loremipsum-1.txt
 
-echo "Sed vel mattis turpis. Fusce dapibus, nibh ac porttitor imperdiet, eros eros vulputate odio, vel venenatis dui ipsum sit amet urna. Morbi ac vestibulum felis. Nulla id hendrerit lectus, quis tempor risus. Aenean a leo eros. Proin pretium lacus ac risus malesuada, ut ornare dolor suscipit. Maecenas leo mauris, pellentesque vitae quam ac, gravida euismod lacus. Nullam aliquam ipsum a eros commodo, nec posuere ex luctus. Nunc vel ligula sed risus rutrum pellentesque. "> loremipsum-2.txt
+echo "Sed vel mattis turpis. Fusce dapibus, nibh ac porttitor imperdiet, 
+eros eros vulputate odio, vel venenatis dui ipsum sit amet urna. Morbi ac vestibulum felis. Nulla id hendrerit lectus, quis tempor risus. Aenean a leo eros. Proin pretium
+lacus ac risus malesuada, ut ornare dolor suscipit. Maecenas leo mauris, pellentesque
+vitae quam ac, gravida euismod lacus. Nullam aliquam ipsum a eros commodo, 
+nec posuere ex luctus. Nunc vel ligula sed risus rutrum pellentesque. "> loremipsum-2.txt
 
-echo "Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla rutrum massa ac ex ultrices, sed suscipit tellus tincidunt. Pellentesque felis ante, ornare sit amet massa id, tincidunt mollis metus. Nulla eu condimentum velit, a condimentum nulla. Morbi viverra mi vel enim sodales aliquet a non nibh. Mauris lacinia vehicula nibh, sit amet congue leo hendrerit ac. Ut vel neque in libero finibus auctor non ac velit. Duis eleifend laoreet bibendum. Duis accumsan nec turpis quis pretium. Integer viverra tortor nec aliquam sollicitudin. ">loremipsum-3.txt
+echo "Interdum et malesuada fames ac ante ipsum primis in faucibus. 
+Nulla rutrum massa ac ex ultrices, sed suscipit tellus tincidunt. Pellentesque 
+felis ante, ornare sit amet massa id, tincidunt mollis metus. Nulla eu condimentum velit, 
+a condimentum nulla. Morbi viverra mi vel enim sodales aliquet a non nibh. 
+Mauris lacinia vehicula nibh, sit amet congue leo hendrerit ac. Ut vel neque
+in libero finibus auctor non ac velit. Duis eleifend laoreet bibendum. 
+Duis accumsan nec turpis quis pretium. Integer viverra tortor nec aliquam sollicitudin. ">loremipsum-3.txt
 
-echo "Mauris varius diam sed ex consequat fringilla vel eget sem. Suspendisse gravida ante ac tortor tincidunt, non faucibus ante pretium. Cras commodo viverra sem ut pretium. In viverra, mauris eget eleifend feugiat, metus nunc rutrum tortor, sed tristique purus orci sagittis dui. Maecenas at nibh commodo, interdum sem vel, aliquet nisi. Pellentesque ullamcorper diam id ex tempor, quis auctor metus hendrerit. Aenean et arcu odio. Proin a porta elit. Aenean egestas porttitor odio. Curabitur sed lacinia ipsum. Vivamus justo nulla, bibendum ac fermentum sed, scelerisque sit amet enim. Sed porta erat felis, at lobortis nisl aliquam eget. Curabitur aliquet erat vel augue viverra, at efficitur enim eleifend. Pellentesque accumsan vel ex at accumsan. Proin lacinia, ipsum eget dignissim pretium, felis elit lobortis nisi, nec vestibulum leo diam eget leo. ">loremipsum-4.txt
+echo "Mauris varius diam sed ex consequat fringilla vel eget sem. Suspendisse
+gravida ante ac tortor tincidunt, non faucibus ante pretium. Cras commodo viverra sem ut pretium. In viverra, mauris eget eleifend feugiat, metus nunc rutrum tortor, 
+sed tristique purus orci sagittis dui. Maecenas at nibh commodo, interdum sem vel, aliquet
+nisi. Pellentesque ullamcorper diam id ex tempor, quis auctor metus hendrerit. Aenean 
+et arcu odio. Proin a porta elit. Aenean egestas porttitor odio. Curabitur sed lacinia 
+ipsum. Vivamus justo nulla, bibendum ac fermentum sed, scelerisque sit amet enim. Sed porta
+erat felis, at lobortis nisl aliquam eget. Curabitur aliquet erat vel augue viverra, at efficitur enim eleifend. Pellentesque accumsan vel ex at accumsan. Proin lacinia, 
+ipsum eget dignissim pretium, felis elit lobortis nisi, nec vestibulum leo diam eget leo. ">loremipsum-4.txt
 
-echo "Donec commodo lacinia dolor in laoreet. Suspendisse non vestibulum libero. Maecenas placerat sollicitudin odio. Suspendisse potenti. Curabitur efficitur, erat in lacinia molestie, lacus felis vestibulum enim, et volutpat lacus risus a justo. Proin ac rutrum nisl. Integer ullamcorper, metus vitae porttitor viverra, enim quam cursus enim, id dapibus erat neque eu libero. Duis lectus velit, scelerisque a leo sed, vestibulum finibus est. Fusce nibh est, accumsan sed accumsan in, consectetur et nunc. Phasellus sit amet turpis sit amet mi tristique suscipit. Phasellus mi nibh, imperdiet et cursus nec, facilisis sed quam. Cras facilisis ligula iaculis pretium commodo. Pellentesque eu urna suscipit, tincidunt justo lobortis, laoreet sapien. Sed facilisis elementum purus quis finibus. ">loremipsum-5.txt 
+echo "Donec commodo lacinia dolor in laoreet. Suspendisse non vestibulum libero. 
+Maecenas placerat sollicitudin odio. Suspendisse potenti. Curabitur efficitur, erat in 
+lacinia molestie, lacus felis vestibulum enim, et volutpat lacus risus a justo. Proin 
+ac rutrum nisl. Integer ullamcorper, metus vitae porttitor viverra, enim quam cursus enim, 
+id dapibus erat neque eu libero. Duis lectus velit, scelerisque a leo sed, vestibulum 
+finibus est. Fusce nibh est, accumsan sed accumsan in, consectetur et nunc. Phasellus 
+sit amet turpis sit amet mi tristique suscipit. Phasellus mi nibh, imperdiet et cursus 
+nec, facilisis sed quam. Cras facilisis ligula iaculis pretium commodo. Pellentesque 
+eu urna suscipit, tincidunt justo lobortis, laoreet sapien. Sed facilisis elementum 
+purus quis finibus. ">loremipsum-5.txt 
